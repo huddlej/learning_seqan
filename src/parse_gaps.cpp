7a49f31c5e63c71f50b83a621a9cd0d7fdabb3bd
John Huddleston <huddlej@gmail.com>
2016-08-14 14:24:21 -0700

    added command line interface for gap parser. closes #1.

--- src/parse_gaps.cpp
+++ src/parse_gaps.cpp
@@ -2,7 +2,22 @@
    Parse gaps in a given BAM alignment and output the resulting gaps in BED or
    VCF formats.
  */
+#include <string>
+#include <tclap/CmdLine.h>
 
 int main(int argc, char** argv) {
+    try {
+        TCLAP::CmdLine cmd("Parse gaps in BAM alignments", ' ', "0.1");
+        TCLAP::UnlabeledValueArg<std::string> arg_reference("reference", "path to a FASTA file for the reference used by the given BAM", true, "", "reference");
+        TCLAP::UnlabeledValueArg<std::string> arg_bam("bam", "path to a BAM file to parse", true, "", "bam");
+
+        cmd.add(arg_reference);
+        cmd.add(arg_bam);
+        cmd.parse(argc, argv);
+    }
+    catch (TCLAP::ArgException &e) {
+        std::cerr << "Error: " << e.error() << " for arg " << e.argId() << std::endl;
+    }
+
     return 0;
 }
