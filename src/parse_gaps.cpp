f4d1c35f60558f478ec04c515d164c66872af9d3
John Huddleston <huddlej@gmail.com>
2016-08-14 14:55:28 -0700

    open reference FASTA and BAM prior to parsing. closes #2.

--- src/parse_gaps.cpp
+++ src/parse_gaps.cpp
@@ -4,6 +4,11 @@
  */
 #include <string>
 #include <tclap/CmdLine.h>
+#include <seqan/sequence.h>
+#include <seqan/seq_io.h>
+#include <seqan/bam_io.h>
+
+using namespace seqan;
 
 int main(int argc, char** argv) {
     try {
@@ -14,6 +19,26 @@ int main(int argc, char** argv) {
         cmd.add(arg_reference);
         cmd.add(arg_bam);
         cmd.parse(argc, argv);
+
+        /**
+           Open the FASTA file for the given reference.
+         */
+        std::string reference_file_name = arg_reference.getValue();
+        FaiIndex fai_index;
+        if (!open(fai_index, toCString(reference_file_name))) {
+            std::cerr << "Error: Could not open FASTA index for reference: " << reference_file_name << std::endl;
+            return 1;
+        }
+
+        /**
+           Open the BAM file of alignments to the given reference.
+         */
+        std::string bam_file_name = arg_bam.getValue();
+        BamFileIn bam_file_in;
+        if (!open(bam_file_in, toCString(bam_file_name))) {
+            std::cerr << "Error: Could not open BAM: " << bam_file_name << std::endl;
+            return 1;
+        }
     }
     catch (TCLAP::ArgException &e) {
         std::cerr << "Error: " << e.error() << " for arg " << e.argId() << std::endl;
